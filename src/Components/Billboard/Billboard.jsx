f6d8c0cfb8c113effc117278e768701030a1f177
HarshiniM18 <154896188+HarshiniM18@users.noreply.github.com>
2024-03-05 13:00:29 +0530

    Add files via upload

---
+++ src/Components/Billboard/Billboard.jsx
@@ -0,0 +1,7 @@
+export default function Billboard() {
+  return (
+    <div className="billboard-container">
+      <img src="https://www.c-metric.com/wp-content/uploads/2019/08/Microsoft-SharePoint-Development.jpg" />
+    </div>
+  );
+}
